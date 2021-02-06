b0ab7def3ee52e5faf61760963162da9b00d9b72
souvik1982 <souvik1982@gmail.com>
2021-02-05 22:32:08 -0500

    Added an ERROR for DB connection failure.

--- dbconnect.php
+++ dbconnect.php
@@ -6,7 +6,7 @@ function openConnection()
   $dbuser = "souvik";
   $dbpass = "MuayThai23";
   $db = "practice";
-  $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
+  if (!$conn = new mysqli($dbhost, $dbuser, $dbpass, $db)) echo "<b>ERROR</b>: Could not connect to the mySQL database. Please contact database management. </br> \n";
   return $conn;
 }
 
