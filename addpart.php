09183f25bc75f29c5e0ee473e31ef2d265999f14
Souvik Das <souvik@cos-10-164-19-89.science.purdue.edu>
2021-02-10 21:55:10 -0500

    Checking for POST before unpacking FILES

--- addpart.php
+++ addpart.php
@@ -36,95 +36,98 @@
 
 <?php
 
-  $N_PDF = count($_FILES["PDFToUpload"]["name"]);
-  $N_CSV = count($_FILES["CSVToUpload"]["name"]);
-
-  include("dbconnect.php");
-  $connection = openConnection();
-
-  // Iterate over the PDF and CSV lists to find files that correspond to each other
-  // This is an expensive N^2/2 operation
-  for ($i_PDF = 0; $i_PDF < $N_PDF; $i_PDF++)
+  if (isset($_POST["submit"]))
   {
-    $filename_PDF = $_FILES["PDFToUpload"]["name"][$i_PDF];
-    $sheetstring = substr($filename_PDF, 0, strlen($filename_PDF)-4);
+    $N_PDF = count($_FILES["PDFToUpload"]["name"]);
+    $N_CSV = count($_FILES["CSVToUpload"]["name"]);
 
-    // There ought to be checks on the sheetstring
+    include("dbconnect.php");
+    $connection = openConnection();
 
-
-    // Check if this sheetstring already exists in the database
-    // If it does, throw an ERROR and continue to next sheet
-    $sqlQuery = "SELECT * from sheets where sheetstring ='".$sheetstring."'";
-    $map_output = mysqli_fetch_assoc(mysqli_query($connection, $sqlQuery));
-    if ($map_output["sheetstring"] == $sheetstring)
+    // Iterate over the PDF and CSV lists to find files that correspond to each other
+    // This is an expensive N^2/2 operation
+    for ($i_PDF = 0; $i_PDF < $N_PDF; $i_PDF++)
     {
-      echo "<b>ERROR</b>: A sheet with identifier ".$sheetstring." had been added on ".$map_output["created_at"]." and therefore cannot be added again. Please contact database management.<br/> \n";
-      continue;
-    }
+      $filename_PDF = $_FILES["PDFToUpload"]["name"][$i_PDF];
+      $sheetstring = substr($filename_PDF, 0, strlen($filename_PDF)-4);
 
-    // Check for CSV partner file
-    $foundCSV = false;
-    for ($i_CSV = 0; $i_CSV < $N_CSV && $foundCSV == false; $i_CSV++)
-    {
-      $filename_CSV = $_FILES["CSVToUpload"]["name"][$i_CSV];
-      if ($sheetstring == substr($filename_CSV, 0, strlen($filename_CSV)-4)) // The filenames match
+      // There ought to be checks on the sheetstring
+
+
+      // Check if this sheetstring already exists in the database
+      // If it does, throw an ERROR and continue to next sheet
+      $sqlQuery = "SELECT * from sheets where sheetstring ='".$sheetstring."'";
+      $map_output = mysqli_fetch_assoc(mysqli_query($connection, $sqlQuery));
+      if ($map_output["sheetstring"] == $sheetstring)
       {
-        $foundCSV = true;
-        $storageDirectory = "../CMSTrackerFlatSheetsDB_Data/".$sheetstring."/";
-
-        // Check if storage directory already exists
-        // If it does, throw an ERROR and continue to the next sheet
-        if (is_dir($storageDirectory))
-        {
-          echo "<b>ERROR</b>: A folder with name ".$storageDirectory." found but database entry for ".$sheetstring." does not exist. Will not add this sheet to the database. Please contact database management.<br/> \n";
-          continue;
-        }
-        // If you cannot make the storage directory, this could be a disk issue.
-        // Throw an ERROR and continue to the next sheet
-        if (!mkdir($storageDirectory))
-        {
-          echo "<b>ERROR</b>: Could not create the folder ".$storageDirectory.". Will not add this sheet to the database. Please contact database management.<br/> \n";
-          continue;
-        }
-
-        $storageFilename_PDF = $storageDirectory.basename($filename_PDF);
-        $storageFilename_CSV = $storageDirectory.basename($filename_CSV);
-        // Store both files in folder and add entry to database/
-        if (move_uploaded_file($_FILES["PDFToUpload"]["tmp_name"][$i_PDF], $storageFilename_PDF) &&
-            move_uploaded_file($_FILES["CSVToUpload"]["tmp_name"][$i_CSV], $storageFilename_CSV))
-        {
-
-          // Parse CSV file to extract thickness
-          if ($handle_CSV = fopen($storageFilename_CSV, "r"))
-          {
-            $thickness = 999;
-            while ($line = fgetcsv($handle_CSV))
-              if ($line[0] == "Mean_thickness")
-              {
-                $thickness = floatval($line[1]*1000);
-                break;
-              }
-
-            $userId = $_SESSION["userId"];
-            $dateTime = date("Y-m-d H:i:s");
-            $location = $_SESSION["affiliation"];
-            $sqlQuery = "INSERT INTO sheets (sheetstring, folder, created_at, userId, thickness, location)
-                         VALUES ('".$sheetstring."',
-                                 '".$storageDirectory."',
-                                 '".$dateTime."',
-                                 '".$userId."',
-                                 '".$thickness."',
-                                 '".$location."')";
-            $output = mysqli_query($connection, $sqlQuery);
-            echo "<b>LOG</b>: Flat sheet ".$sheetstring." PDF and CSV uploaded. Database entry created.<br/> \n";
-          }
-          else echo "<b>WARNING</b>: Could not open CSV file to extract thickness.</br/> \n";
-        }
-        else echo "<b>ERROR</b>: Could not store PDF and CSV files in ".$storageDirectory.". Please contact database management.<br/> \n";
+        echo "<b>ERROR</b>: A sheet with identifier ".$sheetstring." had been added on ".$map_output["created_at"]." and therefore cannot be added again. Please contact database management.<br/> \n";
+        continue;
       }
-    } // For loop over the CSV files
-    if ($foundCSV == false) echo "<b>WARNING</b>: Could not find CSV file for ".$sheetstring.". Neither the PDF nor the CSV file will be uploaded. The database will not be populated with this sheet. Please return with the CSV file.<br/> \n";
-  } // For loop over the PDF files
+
+      // Check for CSV partner file
+      $foundCSV = false;
+      for ($i_CSV = 0; $i_CSV < $N_CSV && $foundCSV == false; $i_CSV++)
+      {
+        $filename_CSV = $_FILES["CSVToUpload"]["name"][$i_CSV];
+        if ($sheetstring == substr($filename_CSV, 0, strlen($filename_CSV)-4)) // The filenames match
+        {
+          $foundCSV = true;
+          $storageDirectory = "../CMSTrackerFlatSheetsDB_Data/".$sheetstring."/";
+
+          // Check if storage directory already exists
+          // If it does, throw an ERROR and continue to the next sheet
+          if (is_dir($storageDirectory))
+          {
+            echo "<b>ERROR</b>: A folder with name ".$storageDirectory." found but database entry for ".$sheetstring." does not exist. Will not add this sheet to the database. Please contact database management.<br/> \n";
+            continue;
+          }
+          // If you cannot make the storage directory, this could be a disk issue.
+          // Throw an ERROR and continue to the next sheet
+          if (!mkdir($storageDirectory))
+          {
+            echo "<b>ERROR</b>: Could not create the folder ".$storageDirectory.". Will not add this sheet to the database. Please contact database management.<br/> \n";
+            continue;
+          }
+
+          $storageFilename_PDF = $storageDirectory.basename($filename_PDF);
+          $storageFilename_CSV = $storageDirectory.basename($filename_CSV);
+          // Store both files in folder and add entry to database/
+          if (move_uploaded_file($_FILES["PDFToUpload"]["tmp_name"][$i_PDF], $storageFilename_PDF) &&
+              move_uploaded_file($_FILES["CSVToUpload"]["tmp_name"][$i_CSV], $storageFilename_CSV))
+          {
+
+            // Parse CSV file to extract thickness
+            if ($handle_CSV = fopen($storageFilename_CSV, "r"))
+            {
+              $thickness = 999;
+              while ($line = fgetcsv($handle_CSV))
+                if ($line[0] == "Mean_thickness")
+                {
+                  $thickness = floatval($line[1]*1000);
+                  break;
+                }
+
+              $userId = $_SESSION["userId"];
+              $dateTime = date("Y-m-d H:i:s");
+              $location = $_SESSION["affiliation"];
+              $sqlQuery = "INSERT INTO sheets (sheetstring, folder, created_at, userId, thickness, location)
+                           VALUES ('".$sheetstring."',
+                                   '".$storageDirectory."',
+                                   '".$dateTime."',
+                                   '".$userId."',
+                                   '".$thickness."',
+                                   '".$location."')";
+              $output = mysqli_query($connection, $sqlQuery);
+              echo "<b>LOG</b>: Flat sheet ".$sheetstring." PDF and CSV uploaded. Database entry created.<br/> \n";
+            }
+            else echo "<b>WARNING</b>: Could not open CSV file to extract thickness.</br/> \n";
+          }
+          else echo "<b>ERROR</b>: Could not store PDF and CSV files in ".$storageDirectory.". Please contact database management.<br/> \n";
+        }
+      } // For loop over the CSV files
+      if ($foundCSV == false) echo "<b>WARNING</b>: Could not find CSV file for ".$sheetstring.". Neither the PDF nor the CSV file will be uploaded. The database will not be populated with this sheet. Please return with the CSV file.<br/> \n";
+    } // For loop over the PDF files
+  }
 
 ?>
 
