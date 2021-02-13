7fbfbc36e986636da9f7a1c5837fada22661ceed
Souvik Das <souvik@cos-10-164-19-124.science.purdue.edu>
2021-02-12 19:05:05 -0500

    Moved Javascript to its own file.

--- searchpart.php
+++ searchpart.php
@@ -5,33 +5,7 @@
 </head>
 <body>
 
-<script>
-  function showMovingElements(id)
-  {
-    var id_edit = "edit_" + id;
-    var id_dropDown = "dropDown_" + id;
-    var id_submit = "submit_" + id;
-    var id_cancel = "cancel_" + id;
-
-    document.getElementById(id_edit).style.display = 'none';
-    document.getElementById(id_dropDown).style.display = 'block';
-    document.getElementById(id_submit).style.display = 'block';
-    document.getElementById(id_cancel).style.display = 'block';
-  }
-
-  function hideMovingElements(id)
-  {
-    var id_edit = "edit_" + id;
-    var id_dropDown = "dropDown_" + id;
-    var id_submit = "submit_" + id;
-    var id_cancel = "cancel_" + id;
-
-    document.getElementById(id_edit).style.display = 'block';
-    document.getElementById(id_dropDown).style.display = 'none';
-    document.getElementById(id_submit).style.display = 'none';
-    document.getElementById(id_cancel).style.display = 'none';
-  }
-</script>
+<script src="showHideElements.js"></script>
 
 <h1 align="center"> CMS Tracker Flat Sheets Database </h1>
 
